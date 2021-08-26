7fb49eec56e99e53887e849f56f94215860a88e6
saifeddine1992 <saifeddinebouazizi1992@gmail.com>
2021-08-26 13:06:09 +0100

    SCD type-2 using Window Functions

--- src/main/scala/com/databeans/AddressHistoryBuilder.scala
+++ src/main/scala/com/databeans/AddressHistoryBuilder.scala
@@ -5,17 +5,13 @@ import org.apache.spark.sql.DataFrame
 
 object AddressHistoryBuilder {
   def buildHistory(history: DataFrame, update: DataFrame): DataFrame = {
-
-
     val rowsBeforeUpdates = joinHistoryAndUpdate(history, update)
     val touchedHistory = getTouchedHistory(rowsBeforeUpdates)
-
     val rowsWithCorrectedMovedOut = ModifyDates(touchedHistory)
     val rowsWithTangentIntervalsOnSameAddress = getTangentIntervalsOnSameAddress(rowsWithCorrectedMovedOut)
     val unifiedTangentIntervals = unifyTangentIntervals(rowsWithTangentIntervalsOnSameAddress)
     val separatedTangentIntervals = removeSeparatedTangentIntervals(rowsWithTangentIntervalsOnSameAddress)
     val untouchedHistory = getUntouchedHistory(rowsBeforeUpdates)
-
 
     val correctedDates = rowsWithCorrectedMovedOut
       .except(separatedTangentIntervals)
